036f309bae6eefc709485acf312c4cbf93436e76
sahildhami007 <113689477+sahildhami007@users.noreply.github.com>
2022-11-01 16:44:03 +0530

    game finish

--- game.js
+++ game.js
@@ -4,23 +4,26 @@ let num3 = document.getElementById('num3');
 let num4 = document.getElementById('num4');
 let num5 = document.getElementById('num5');
 let num6 = document.getElementById('num6');
-let inputValues = [];
 let newNumber;
 let timer;
 let i = 15;
 let totalCount;
+let currentArray = [];
+let arrayVal = [];
 
 function play() {
+    clearForm()
+    setTimeout(() => {
+        document.querySelector('#counter').style.display = 'block';
+        document.querySelector('#select-btn').disabled = false;
+    }, 1000)
+
     document.querySelector('#section1').style.display = 'block';
     document.querySelector('#section2').style.display = 'block';
     document.querySelector('#section3').style.display = 'none';
     document.querySelector('#play').style.display = 'none';
     i = 15;
     startCount();
-}
-
-function playAgain() {
-    window.location.reload();
 }
 
 function startCount() {
@@ -33,18 +36,18 @@ function startCount() {
 
         if (i < 0) {
             clearInterval(timer);
-            console.log('interval cleared');
+            document.querySelector('#select-btn').disabled = true;
             totalCount = 14 - i;
-            console.log("totalCount is: " + totalCount);
             document.querySelector('#section1').style.display = 'none';
             document.querySelector('#section2').style.display = 'none';
             document.querySelector('#section3').style.display = 'block';
             storeValues();
         }
-    }, 500);
+    }, 1000);
 }
 
 function select() {
+
     if (num1.value.length === 0) {
         num1.value = newNumber;
     } else if (num2.value.length === 0) {
@@ -59,24 +62,61 @@ function select() {
         num6.value = newNumber;
 
         clearInterval(timer);
-        console.log('interval cleared');
+        document.querySelector('#select-btn').disabled = true;
         totalCount = 14 - i;
-        console.log("totalCount is: " + totalCount);
         setTimeout(() => {
             document.querySelector('#section1').style.display = 'none';
             document.querySelector('#section2').style.display = 'none';
             document.querySelector('#section3').style.display = 'block';
-        }, 800)
-        storeValues();
+            storeValues();
+
+        }, 300)
     }
 }
 
 function storeValues() {
+
     var inputs = document.getElementsByTagName("input");
-    for (var s = 0; s < inputs.length; s++) {
-        inputValues.push(inputs[s].value);
+    for (s = 0; s < inputs.length; s++) {
+        currentArray.push(inputs[s].value);
+        var set = new Set(currentArray);
+        if (set.size === 6) {
+            console.log("unique " + true);
+            currentArray.push(totalCount);
+            arrayVal.push(currentArray);
+            totalCount = "";
+        }
     }
-    inputValues.push("count: " + totalCount );
-    console.log(inputValues);
+    console.log(currentArray)
+    console.log(arrayVal);
+    clearForm();
 }
+
+function clearForm() {
+    num1.value = "";
+    num2.value = "";
+    num3.value = "";
+    num4.value = "";
+    num5.value = "";
+    num6.value = "";
+    totalCount = "";
+    document.getElementById('counter').innerHTML = "";
+    currentArray = [];
+}
+
+// function getWinner() {
+//     const vals = [
+//         ['2', '1', '5', '4', '6', '3', 11],
+//         ['4', '6', '3', '1', '2', '5', 9],
+//         ['6', '2', '4', '1', '3', '5', 10],
+//         ['6', '2', '4', '1', '3', '5', 7],
+//         ['6', '2', '4', '1', '3', '5', 14]
+//     ];
+//     console.log(vals);
+
+//     for (let q = 0; q < vals.length; q++) {
+//         let time = vals[q][6];
+//         console.log(time);
+//     }
+// }
 
